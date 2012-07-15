02c74b6634f7df97291d6fc1a47eeae7e0d51fcb
Ian Cullen <ian.james.cullen@gmail.com>
2012-07-15 19:12:56 +0100

    When colouring by 1st isomer the ground state is now not drawn for those isotopes with an isomer

--- src/draw_nuclei.cpp
+++ src/draw_nuclei.cpp
@@ -5,7 +5,8 @@ void draw_nuclei(std::vector<Nuclide> &in,
 		 std::vector<float> &n,
 		 std::vector<bool> &k,
 		 inputs *draw,
-		 //File is already opened in write_[EPS,SVG]() so pass open stream in rather than openinf file again.
+		 //File is already opened in write_[EPS,SVG]()
+		 //so pass open stream in rather than opening the file again.
 		 std::ofstream &out_file
 		 )
 {
@@ -286,22 +287,16 @@ void draw_nuclei(std::vector<Nuclide> &in,
 		      k[5]=1;
 		    }
 		}
-	      else if (nuc_it->st == 0
-		       && (nuc_it+1)->Z != nuc_it->Z
-		       && ((nuc_it+1)->A == nuc_it->A || (nuc_it-1)->A == nuc_it->A)
-		       //&& (nuc_it+1)->st == 1
-		       //     || (nuc_it-1)->A == nuc_it->A
-		       //    )
-		       )
+	      else if (nuc_it->st == 0 && (nuc_it+1)->st != 1)
 		{
 		  //-As not every nucleus has an isomer, draw empty boxes as a visual aid
 		  kcol.at(6) = "white";
 		  colour = kcol.at(6);
 		  k[6]=1;
-		  
+
 		  if (nuc_it->decay=="stable")
 		    colour="black";
-		  
+
 		  if (draw->file_type == 0)
 		    {
 		      out_file << "%-" << nuc_it->A << nuc_it->symbol << "-\n"
@@ -322,7 +317,7 @@ void draw_nuclei(std::vector<Nuclide> &in,
 			  else
 			    out_file << " black";
 
-			  out_file << " (" << nuc_it->symbol << ") (" << nuc_it->A << ") ";
+			  out_file << " (" << nuc_it->symbol << ") (" << nuc_it->A << ")";
 			}
 
 		      out_file << " " << colour
@@ -402,7 +397,7 @@ void draw_nuclei(std::vector<Nuclide> &in,
 		      else
 			out_file << " black";
 
-		      out_file << " (" << nuc_it->symbol << ") (" << nuc_it->A << ") ";
+		      out_file << " (" << nuc_it->symbol << ") (" << nuc_it->A << ")";
 		    }
 
 		  out_file << " " << colour

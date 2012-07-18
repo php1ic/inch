6a47125a6ed9552568404ce09bc4906ec856dcdf
Ian Cullen <ian.james.cullen@gmail.com>
2012-07-18 22:21:27 +0100

    Change contact details in the TODO file

--- src/inputs.cpp
+++ src/inputs.cpp
@@ -20,17 +20,17 @@ inputs::inputs()
 
   e_or_t=0;
 
-  size       = 13;  // See comment above
+  size       = 6;  // See comment above
 
-  drip_lines = 0;  // 0=none, 1=both, 2=p-only, 3=n-only
+  drip_lines = 1;  // 0=none, 1=both, 2=p-only, 3=n-only
   np_rich    = 1;  // 1=all, 2=p-rich and stable, 3=n-rich and stable, 6=stable only
 
   // off/on - 0/1
   grid          = 0;
   magic_numbers = 1;
   write_isotope = 1;
-  r_process     = 0;
-  key           = 0;
+  r_process     = 1;
+  key           = 1;
   own_nuclei    = 0;
   AME           = 0;
 

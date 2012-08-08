855bbe2a4dc7d93d6b8a8561d31faaa0e7cd3d34
Ian Cullen <ian.james.cullen@gmail.com>
2012-08-08 21:29:21 +0100

    Use a better variable name

--- src/extractValue.cpp
+++ src/extractValue.cpp
@@ -1,17 +1,17 @@
 #include "functions.h"
 
-template <typename Type> 
+template <typename Type>
 void extractValue(std::string *line, int start, int end, Type &value)
 {
-  std::stringstream test;
+  std::stringstream in;
   std::string *temp = new std::string;
-  
-  temp->assign(line->begin()+start,line->begin()+end);
-  test << *temp;
-  test >> value;
 
-  test.str("");
-  test.clear();
-  
+  temp->assign(line->begin()+start,line->begin()+end);
+  in << *temp;
+  in >> value;
+
+  in.str("");
+  in.clear();
+
   delete temp;
 }

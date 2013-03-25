b3ac80793a906c6252cf43f0ab131891011641c6
Ian Cullen <ian.james.cullen@gmail.com>
2013-03-25 22:14:27 +0000

    Fully remove a commented out lines.

--- src/inputs.cpp
+++ src/inputs.cpp
@@ -34,7 +34,7 @@ inputs::inputs()
   write_isotope = 1;
   r_process     = 1;
   key           = 1;
-  own_nuclei    = 0;
+  own_nuclei    = 1;
   AME           = 0;
 
   curve = 0.25;  // 0->1=square->circle

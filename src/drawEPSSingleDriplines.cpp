80dbd5c7bec2cd64292c3c01ef01723631cd26c2
Ian Cullen <ian.james.cullen@gmail.com>
2014-04-21 21:42:06 +0100

    Filename didn't match the function name.

---
+++ src/drawEPSSingleDriplines.cpp
@@ -0,0 +1,24 @@
+#include "include/functions.h"
+
+void drawEPSSingleDriplines(const std::vector<Nuclide> &nuc,
+			    inputs *draw,
+			    std::ofstream &out_file
+			    )
+{
+  //-May only want one of the drip lines
+  //------------------------------------------
+  // single_drip_lines = 1 -> both
+  // single_drip_lines = 2 -> only p drip line
+  // single_drip_lines = 3 -> only n drip line
+  //------------------------------------------
+
+  if (draw->single_drip_lines != 3)
+    drawEPSSingleProtonDripline(nuc, draw, out_file);
+  else
+    std::cout << "Not drawing the one-proton drip line" << std::endl;
+
+  if (draw->single_drip_lines != 2)
+    drawEPSSingleNeutronDripline(nuc, draw, out_file);
+  else
+    std::cout << "Not drawing the one-neutron drip line" << std::endl;
+}

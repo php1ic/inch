b18516994b091ddf24a03df376e9ac0901e0705c
Ian Cullen <ian.james.cullen@gmail.com>
2014-07-27 21:41:14 +0100

    Remove unnecessary use of temp variable and alter source code formatting.

--- src/setKeyScale.cpp
+++ src/setKeyScale.cpp
@@ -6,26 +6,26 @@ void setKeyScale(inputs *draw,
 {
   if (!draw->key)
     {
-      draw->key_scale=0;
+      draw->key_scale=0.0;
       return;
     }
 
   for (unsigned int i=0; i<part->draw.size(); ++i)
-    {
-      if (part->draw[part->draw.size()-(i+1)])
-	draw->key_height+=1.5;
-    }
+    if (part->draw[part->draw.size()-(i+1)])
+      draw->key_height+=1.5;
 
-  double scale=0.0;
 
-  //(Bit of a)HACK - We don't want the key to shrink below a certain size.
+  //We don't want the key to shrink below a certain size.
   if (draw->Zmax-draw->Zmin < 9)
-    scale=9/draw->key_height;
+    draw->key_scale=9/draw->key_height;
   else
-    scale=(draw->Zmax-draw->Zmin)/draw->key_height;
+    draw->key_scale=(draw->Zmax-draw->Zmin)/draw->key_height;
 
-  if (scale>3.0 || draw->section=="a" || draw->Zmax-draw->Zmin==MAX_Z)
-    draw->key_scale=3.0;
-  else
-    draw->key_scale=scale;
+  //Nor do we want it to be larger than a certain size.
+  if (   draw->key_scale > 3.0
+      || draw->section == "a"
+      || draw->Zmax-draw->Zmin == MAX_Z)
+    {
+      draw->key_scale=3.0;
+    }
 }

62037a5dde10e0f080780d7720e55430b453c39c
Ian Cullen <ian.james.cullen@gmail.com>
2014-08-02 16:20:10 +0100

    Speed up reading and recording of user defined nuclei.
    There is no need to store the values specified in the user file, so
    as with reading the AME, store info in temp variables then find
    appropriate isotope in the 'main' vector and toggle it's value.

--- src/readOWN.cpp
+++ src/readOWN.cpp
@@ -15,29 +15,33 @@ bool readOWN(const std::string &my_nuclei,
       return false;
     }
 
-  std::string line;
-  std::vector<int> own_N, own_Z, own_IS;
-  std::vector<Nuclide>::iterator nuc_it;
-
   std::ifstream my_nuc(my_nuclei.c_str());
-  std::stringstream in;
 
   if (my_nuc.is_open())
     {
+      std::string line;
+      std::vector<Nuclide>::iterator nuc_it;
+
       while (getline(my_nuc,line))
 	{
 	  if ( !line.compare("") || line.at(0) == '#' )
 	    continue;
 
-	  int N[3];
-	  in.clear();
-	  in << line;
+	  int N=0, Z=0, st=0;
+	  std::stringstream in(line);
+	  in >> N >> Z >> st;
 
-	  in >> N[0] >> N[1] >> N[2];
-
-	  own_N.push_back(N[0]);
-	  own_Z.push_back(N[1]);
-	  own_IS.push_back(N[2]);
+	  for (nuc_it=nuc.begin(); nuc_it!=nuc.end(); ++nuc_it)
+	    {
+	      if(   nuc_it->N  == N
+		 && nuc_it->Z  == Z
+		 && nuc_it->st == st
+		 )
+		{
+		  nuc_it->own = true;
+		  break;
+		}
+	    }
 	}
       my_nuc.close();
     }
@@ -49,25 +53,6 @@ bool readOWN(const std::string &my_nuclei,
       return false;
     }
 
-  //-Set member own to 0 if nuclei not in user's file
-  for (nuc_it=nuc.begin(); nuc_it!=nuc.end(); ++nuc_it)
-    {
-      bool k=false;
-      for (unsigned int i=0; i<own_N.size(); ++i)
-	{
-	  if(    own_N.at(i) == nuc_it->N
-	     &&  own_Z.at(i) == nuc_it->Z
-	     && own_IS.at(i) == nuc_it->st)
-	    {
-	      nuc_it->own = true;
-	      k = true;
-	      break;
-	    }
-	}
-
-      if (!k)
-	nuc_it->own = false;
-    }
   std::cout << "--) done" << std::endl;
 
   return true;

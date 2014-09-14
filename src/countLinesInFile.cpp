52e3be85bcf7b37d7f66998b2e45ee555d0f2129
Ian Cullen <ian.james.cullen@gmail.com>
2014-09-14 22:48:20 +0100

    Continue camelCase convention.

--- src/countLinesInFile.cpp
+++ src/countLinesInFile.cpp
@@ -3,11 +3,11 @@
 int countLinesInFile(std::ifstream &inFile)
 {
   //Get line count
-  int line_count=std::count(std::istreambuf_iterator<char>(inFile),
-			    std::istreambuf_iterator<char>(), '\n');
+  int lineCount=std::count(std::istreambuf_iterator<char>(inFile),
+			   std::istreambuf_iterator<char>(), '\n');
 
   //Counting lines leaves stream pointing to end, put it back to start.
   inFile.seekg(0,inFile.beg);
 
-  return line_count;
+  return lineCount;
 }

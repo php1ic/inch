42701a7334477ba076acb7e305a859a2b7c175d7
Ian Cullen <ian.james.cullen@gmail.com>
2014-09-15 21:48:32 +0100

    Continue camelCase convention.

--- src/drawNuclei.cpp
+++ src/drawNuclei.cpp
@@ -2,114 +2,114 @@
 
 void drawNuclei(std::vector<Nuclide> &in,
 		const inputs *draw,
-		std::ostream &out_file)
+		std::ostream &outFile)
 {
-  std::vector<Nuclide>::iterator nuc_it;
+  std::vector<Nuclide>::iterator it;
 
-  for (nuc_it=in.begin(); nuc_it!=in.end(); ++nuc_it)
+  for (it=in.begin(); it!=in.end(); ++it)
     {
-      if (nuc_it->show == 1)
+      if (it->show == 1)
 	{
 	  if (draw->file_type == 0)
 	    {
-	      out_file << "%" << nuc_it->A << draw->convertZToSymbol(nuc_it->Z) << "\n";
+	      outFile << "%" << it->A << draw->convertZToSymbol(it->Z) << "\n";
 
 	      if (draw->choice=="e"
-		  && nuc_it!=in.begin()
-		  && (nuc_it-1)->N==nuc_it->N
-		  && (nuc_it-1)->Z==nuc_it->Z
-		  && (nuc_it-1)->st==0
-		  && (nuc_it-1)->decay=="stable")
+		  && it!=in.begin()
+		  && (it-1)->N==it->N
+		  && (it-1)->Z==it->Z
+		  && (it-1)->st==0
+		  && (it-1)->decay=="stable")
 		{
-		  out_file << "1";
+		  outFile << "1";
 		}
 	      else
 		{
 		  //-If user specified nuclei are to be drawn,
 		  //-mark with a half square along the diagonal
-		  out_file << (nuc_it->own ? "8" : "0");
+		  outFile << (it->own ? "8" : "0");
 		}
 
 	      if (draw->write_isotope)
 		{
 		  //-If the square is coloured black, change text colour to white
-		  if (nuc_it->colour == "black")
+		  if (it->colour == "black")
 		    {
 		      //-If the square is black and marked as an own nuclei,
 		      //-change text colour to red
-		      out_file << (nuc_it->own ? " red" : " white");
+		      outFile << (it->own ? " red" : " white");
 		    }
 		  else if (draw->choice=="e"
-			   && nuc_it!=in.begin()
-			   && (nuc_it-1)->N==nuc_it->N
-			   && (nuc_it-1)->Z==nuc_it->Z
-			   && (nuc_it-1)->st==0
-			   && (nuc_it-1)->decay=="stable")
+			   && it!=in.begin()
+			   && (it-1)->N==it->N
+			   && (it-1)->Z==it->Z
+			   && (it-1)->st==0
+			   && (it-1)->decay=="stable")
 		    {
-		      out_file << " white";
+		      outFile << " white";
 		    }
 		  else
 		    {
-		      out_file << " black";
+		      outFile << " black";
 		    }
 
-		  out_file << " (" << draw->convertZToSymbol(nuc_it->Z) << ") (" << nuc_it->A << ")";
+		  outFile << " (" << draw->convertZToSymbol(it->Z) << ") (" << it->A << ")";
 		}
 
-	      out_file << " " << nuc_it->colour
-		       << " " << nuc_it->N-draw->Nmin
-		       << " " << nuc_it->Z-draw->Zmin
-		       << " curve Nucleus" << std::endl;
+	      outFile << " " << it->colour
+		      << " " << it->N-draw->Nmin
+		      << " " << it->Z-draw->Zmin
+		      << " curve Nucleus" << std::endl;
 	    }
 	  else if (draw->file_type == 1)
 	    {
-	      out_file << "<!--" << nuc_it->A << draw->convertZToSymbol(nuc_it->Z) << "-->\n";
+	      outFile << "<!--" << it->A << draw->convertZToSymbol(it->Z) << "-->\n";
 
-	      out_file << "<g transform=\"translate(" << nuc_it->N-draw->Nmin << " " << draw->Zmax-nuc_it->Z << ")\"> "
-		       << "<use xlink:href=\"#" << nuc_it->colour << "Nucleus\"/></g>" << std::endl;
-	      //<< "<text class=\"MidSymbol Black\" dx=\"0.5\" dy=\"0.80\">" << draw->convertZToSymbol(nuc_it->Z) << "</text> "
-	      //<< "<text class=\"MidNumber Black\" dx=\"0.5\" dy=\"0.35\">" << nuc_it->A << "</text></g>" << std::endl;
+	      outFile << "<g transform=\"translate(" << it->N-draw->Nmin << " " << draw->Zmax-it->Z << ")\"> "
+		      << "<use xlink:href=\"#" << it->colour << "Nucleus\"/></g>" << std::endl;
+	      //<< "<text class=\"MidSymbol Black\" dx=\"0.5\" dy=\"0.80\">" << draw->convertZToSymbol(it->Z) << "</text> "
+	      //<< "<text class=\"MidNumber Black\" dx=\"0.5\" dy=\"0.35\">" << it->A << "</text></g>" << std::endl;
 	    }
 	  else if (draw->file_type == 2)
 	    {
-	      out_file << "%" << nuc_it->A << draw->convertZToSymbol(nuc_it->Z) << "\n";
+	      outFile << "%" << it->A << draw->convertZToSymbol(it->Z) << "\n";
 
-	      out_file << "\\nucleus{" << nuc_it->colour << "}{"
-		       << nuc_it->N << "}{"
-		       << nuc_it->Z << "}{"
-		       << nuc_it->A << "}{"
-		       << draw->convertZToSymbol(nuc_it->Z) << "}" << std::endl;
+	      outFile << "\\nucleus{" << it->colour << "}{"
+		      << it->N << "}{"
+		      << it->Z << "}{"
+		      << it->A << "}{"
+		      << draw->convertZToSymbol(it->Z) << "}" << std::endl;
 	    }
 	}
-      else if (nuc_it->show == 2)
+      else if (it->show == 2)
 	{
-	  if (nuc_it->decay=="stable")
-	    nuc_it->colour="black";
+	  if (it->decay=="stable")
+	    it->colour="black";
 
 	  if (draw->file_type == 0)
 	    {
-	      out_file << "%" << nuc_it->A << draw->convertZToSymbol(nuc_it->Z) << "\n"
-		       << "0";
+	      outFile << "%" << it->A << draw->convertZToSymbol(it->Z) << "\n"
+		      << "0";
 
 	      if (draw->write_isotope)
 		{
 		  //-If the square is coloured black, change text colour to white
-		  if (nuc_it->colour == "black")
+		  if (it->colour == "black")
 		    {
 		      //-If the square is black and marked as an own nuclei,
 		      //-change text colour to red
-		      out_file << (nuc_it->own ? " red" : " white");
+		      outFile << (it->own ? " red" : " white");
 		    }
 		  else
-		    out_file << " black";
+		    outFile << " black";
 
-		  out_file << " (" << draw->convertZToSymbol(nuc_it->Z) << ") (" << nuc_it->A << ")";
+		  outFile << " (" << draw->convertZToSymbol(it->Z) << ") (" << it->A << ")";
 		}
 
-	      out_file << " " << nuc_it->colour
-		       << " " << nuc_it->N-draw->Nmin
-		       << " " << nuc_it->Z-draw->Zmin
-		       << " curve Nucleus" << std::endl;
+	      outFile << " " << it->colour
+		      << " " << it->N-draw->Nmin
+		      << " " << it->Z-draw->Zmin
+		      << " curve Nucleus" << std::endl;
 	    }
 	  else if (draw->file_type == 1)
 	    {

d131161423316fc32d5a956fa651debcbb0774fb
Ian Cullen <ian.james.cullen@gmail.com>
2014-09-15 20:48:29 +0100

    Continue camelCase convention.

--- src/createTIKZProlog.cpp
+++ src/createTIKZProlog.cpp
@@ -1,20 +1,20 @@
 #include "include/functions.h"
 
 void createTIKZProlog(const inputs *draw,
-		      std::ofstream &out_file
+		      std::ofstream &outFile
 		      )
 {
-  out_file << "\\documentclass{article}\n"
-	   << "\\usepackage{tikz}\n"
-	   << "\\usepackage[T1]{fontenc}\n"
-	   << "\\usepackage[active,tightpage]{preview}\n"
-	   << "\\PreviewEnvironment{tikzpicture}\n"
-	   << "\n"
-	   << "%Setup command to draw box and text\n"
-	   << "\\newcommand{\\nucleus}[6][" << draw->curve << "]{\n"
-	   << "\\pgfmathsetmacro{\\rc}{#1*sqrt(200)}\n"
-	   << "\\filldraw[draw=black,thick,fill=#2,rounded corners=\\rc] (#3,#4) rectangle +(1,1)\n"
-	   << "+(0.5,0.75) node[anchor=mid,text=black] {#5}\n"
-	   << "+(0.5,0.27) node[anchor=mid,text=black] {\\Large #6};\n"
-	   << "}\n" << std::endl;
+  outFile << "\\documentclass{article}\n"
+	  << "\\usepackage{tikz}\n"
+	  << "\\usepackage[T1]{fontenc}\n"
+	  << "\\usepackage[active,tightpage]{preview}\n"
+	  << "\\PreviewEnvironment{tikzpicture}\n"
+	  << "\n"
+	  << "%Setup command to draw box and text\n"
+	  << "\\newcommand{\\nucleus}[6][" << draw->curve << "]{\n"
+	  << "\\pgfmathsetmacro{\\rc}{#1*sqrt(200)}\n"
+	  << "\\filldraw[draw=black,thick,fill=#2,rounded corners=\\rc] (#3,#4) rectangle +(1,1)\n"
+	  << "+(0.5,0.75) node[anchor=mid,text=black] {#5}\n"
+	  << "+(0.5,0.27) node[anchor=mid,text=black] {\\Large #6};\n"
+	  << "}\n" << std::endl;
 }

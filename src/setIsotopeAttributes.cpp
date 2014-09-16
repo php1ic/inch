9df0d81a0d06dea2a5de8c06dfb47b1b24cd61b0
Ian Cullen <ian.james.cullen@gmail.com>
2014-09-16 22:07:14 +0100

    More name changing.

--- src/setIsotopeAttributes.cpp
+++ src/setIsotopeAttributes.cpp
@@ -5,292 +5,292 @@ void setIsotopeAttributes(std::vector<Nuclide> &in,
 			  const inputs *draw
 			  )
 {
-  std::vector<Nuclide>::iterator nuc_it;
+  std::vector<Nuclide>::iterator it;
 
   // Using the region specified, flag that the isotope should be drawn
   // together with the corresponding part of the key.
-  for (nuc_it=in.begin(); nuc_it!=in.end(); ++nuc_it)
+  for (it=in.begin(); it!=in.end(); ++it)
     {
-      if (   nuc_it->Z    >= draw->Zmin
-	  && nuc_it->Z    <= draw->Zmax
-	  && nuc_it->N    >= draw->Nmin
-	  && nuc_it->N    <= draw->Nmax
-	  && nuc_it->exp  != draw->experimental
-	  && nuc_it->rich  % draw->np_rich == 0
+      if (   it->Z    >= draw->Zmin
+	  && it->Z    <= draw->Zmax
+	  && it->N    >= draw->Nmin
+	  && it->N    <= draw->Nmax
+	  && it->exp  != draw->experimental
+	  && it->rich  % draw->np_rich == 0
 	  )
 	{
 	  // Error on mass excess units of keV
 	  if (draw->choice == "a")
 	    {
-	      if (nuc_it->st == 0)
+	      if (it->st == 0)
 		{
-		  nuc_it->show=1;
+		  it->show=1;
 
-		  double me = draw->AME ? nuc_it->AME_dME : nuc_it->NUBASE_dME;
+		  double me = draw->AME ? it->AME_dME : it->NUBASE_dME;
 
-		  if (nuc_it->decay == "stable" && me <= part->value[0])
+		  if (it->decay == "stable" && me <= part->value[0])
 		    {
-		      nuc_it->colour = part->colour[0];
+		      it->colour = part->colour[0];
 		      part->draw[0]=true;
 		    }
-		  else if (nuc_it->decay == "stable" && me > part->value[0])
+		  else if (it->decay == "stable" && me > part->value[0])
 		    {
-		      nuc_it->colour = part->colour[1];
+		      it->colour = part->colour[1];
 		      part->draw[1]=true;
 		    }
 		  else if (me <= part->value[0])
 		    {
-		      nuc_it->colour = part->colour[2];
+		      it->colour = part->colour[2];
 		      part->draw[2]=true;
 		    }
 		  else if (me > part->value[0] && me <= part->value[1])
 		    {
-		      nuc_it->colour = part->colour[3];
+		      it->colour = part->colour[3];
 		      part->draw[3]=true;
 		    }
 		  else if (me > part->value[1] && me <= part->value[2])
 		    {
-		      nuc_it->colour = part->colour[4];
+		      it->colour = part->colour[4];
 		      part->draw[4]=true;
 		    }
 		  else if (me > part->value[2] && me <= part->value[3])
 		    {
-		      nuc_it->colour = part->colour[5];
+		      it->colour = part->colour[5];
 		      part->draw[5]=true;
 		    }
 		  else if (me > part->value[3] && me <= part->value[4])
 		    {
-		      nuc_it->colour = part->colour[6];
+		      it->colour = part->colour[6];
 		      part->draw[6]=true;
 		    }
 		  else
 		    {
-		      nuc_it->colour = part->colour[7];
+		      it->colour = part->colour[7];
 		      part->draw[7]=true;
 		    }
 		}
 	      else
-		nuc_it->show=0;
+		it->show=0;
 	    }
 	  // Relative error on mass excess units of keV
 	  else if (draw->choice == "b")
 	    {
-	      if (nuc_it->st == 0)
+	      if (it->st == 0)
 		{
-		  nuc_it->show=1;
+		  it->show=1;
 
 		  double dme=0.0, min=1.0e-7;
 
 		  if (draw->AME)
 		    {
-		      if ( fabs(nuc_it->AME_ME) < min)
+		      if ( fabs(it->AME_ME) < min)
 			dme = 0.0;
 		      else
-			dme = fabs(nuc_it->AME_dME/nuc_it->AME_ME);
+			dme = fabs(it->AME_dME/it->AME_ME);
 		    }
 		  else
 		    {
-		      if ( fabs(nuc_it->NUBASE_ME) < min)
+		      if ( fabs(it->NUBASE_ME) < min)
 			dme = 0.0;
 		      else
-			dme = fabs(nuc_it->NUBASE_dME/nuc_it->NUBASE_ME);
+			dme = fabs(it->NUBASE_dME/it->NUBASE_ME);
 		    }
 
 		  if (dme <= part->value[0])
 		    {
-		      nuc_it->colour = part->colour[0];
+		      it->colour = part->colour[0];
 		      part->draw[0]=true;
 		    }
 		  else if (dme > part->value[0] && dme <= part->value[1])
 		    {
-		      nuc_it->colour = part->colour[1];
+		      it->colour = part->colour[1];
 		      part->draw[1]=true;
 		    }
 		  else if (dme > part->value[1] && dme <= part->value[2])
 		    {
-		      nuc_it->colour = part->colour[2];
+		      it->colour = part->colour[2];
 		      part->draw[2]=true;
 		    }
 		  else if (dme > part->value[2] && dme <= part->value[3])
 		    {
-		      nuc_it->colour = part->colour[3];
+		      it->colour = part->colour[3];
 		      part->draw[3]=true;
 		    }
 		  else if (dme > part->value[3] && dme <= part->value[4])
 		    {
-		      nuc_it->colour = part->colour[4];
+		      it->colour = part->colour[4];
 		      part->draw[4]=true;
 		    }
 		  else if (dme > part->value[4] || dme < min)
 		    {
-		      nuc_it->colour = part->colour[5];
+		      it->colour = part->colour[5];
 		      part->draw[5]=true;
 		    }
 		}
 	      else
-		nuc_it->show=0;
+		it->show=0;
 	    }
 	  // Major ground-state decay mode
 	  else if (draw->choice == "c")
 	    {
-	      if (nuc_it->st == 0)
+	      if (it->st == 0)
 		{
-		  nuc_it->show=1;
+		  it->show=1;
 
-		  if (nuc_it->decay == "stable")
+		  if (it->decay == "stable")
 		    {
-		      nuc_it->colour = part->colour[0];
+		      it->colour = part->colour[0];
 		      part->draw[0]=true;
 		    }
-		  else if (nuc_it->decay == "A")
+		  else if (it->decay == "A")
 		    {
-		      nuc_it->colour = part->colour[1];
+		      it->colour = part->colour[1];
 		      part->draw[1]=true;
 		    }
-		  else if (nuc_it->decay == "B-")
+		  else if (it->decay == "B-")
 		    {
-		      nuc_it->colour = part->colour[2];
+		      it->colour = part->colour[2];
 		      part->draw[2]=true;
 		    }
-		  else if (nuc_it->decay == "B+")
+		  else if (it->decay == "B+")
 		    {
-		      nuc_it->colour = part->colour[3];
+		      it->colour = part->colour[3];
 		      part->draw[3]=true;
 		    }
-		  else if (nuc_it->decay == "SF")
+		  else if (it->decay == "SF")
 		    {
-		      nuc_it->colour = part->colour[4];
+		      it->colour = part->colour[4];
 		      part->draw[4]=true;
 		    }
-		  else if (nuc_it->decay == "n")
+		  else if (it->decay == "n")
 		    {
-		      nuc_it->colour = part->colour[5];
+		      it->colour = part->colour[5];
 		      part->draw[5]=true;
 		    }
-		  else if (nuc_it->decay == "2n")
+		  else if (it->decay == "2n")
 		    {
-		      nuc_it->colour = part->colour[6];
+		      it->colour = part->colour[6];
 		      part->draw[6]=true;
 		    }
-		  else if (nuc_it->decay == "p")
+		  else if (it->decay == "p")
 		    {
-		      nuc_it->colour = part->colour[7];
+		      it->colour = part->colour[7];
 		      part->draw[7]=true;
 		    }
-		  else if (nuc_it->decay == "2p")
+		  else if (it->decay == "2p")
 		    {
-		      nuc_it->colour = part->colour[8];
+		      it->colour = part->colour[8];
 		      part->draw[8]=true;
 		    }
-		  else if (nuc_it->decay == "unknown")
+		  else if (it->decay == "unknown")
 		    {
-		      nuc_it->colour = part->colour[9];
+		      it->colour = part->colour[9];
 		      part->draw[9]=true;
 		    }
-		  else if (nuc_it->decay == "EC")
+		  else if (it->decay == "EC")
 		    {
-		      nuc_it->colour = part->colour[10];
+		      it->colour = part->colour[10];
 		      part->draw[10]=true;
 		    }
 		}
 	      else
-		nuc_it->show=0;
+		it->show=0;
 	    }
 	  // Half-life of ground-state
 	  else if (draw->choice == "d")
 	    {
-	      if (nuc_it->st == 0)
+	      if (it->st == 0)
 		{
-		  nuc_it->show=1;
+		  it->show=1;
 
-		  if (nuc_it->hl <= part->value[0])
+		  if (it->hl <= part->value[0])
 		    {
-		      nuc_it->colour = part->colour[0];
+		      it->colour = part->colour[0];
 		      part->draw[0]=true;
 		    }
-		  else if (nuc_it->hl > part->value[0] && nuc_it->hl <= part->value[1])
+		  else if (it->hl > part->value[0] && it->hl <= part->value[1])
 		    {
-		      nuc_it->colour = part->colour[1];
+		      it->colour = part->colour[1];
 		      part->draw[1]=true;
 		    }
-		  else if (nuc_it->hl > part->value[1] && nuc_it->hl <= part->value[2])
+		  else if (it->hl > part->value[1] && it->hl <= part->value[2])
 		    {
-		      nuc_it->colour = part->colour[2];
+		      it->colour = part->colour[2];
 		      part->draw[2]=true;
 		    }
-		  else if (nuc_it->hl > part->value[2] && nuc_it->hl <= part->value[3])
+		  else if (it->hl > part->value[2] && it->hl <= part->value[3])
 		    {
-		      nuc_it->colour = part->colour[3];
+		      it->colour = part->colour[3];
 		      part->draw[3]=true;
 		    }
-		  else if (nuc_it->hl > part->value[3] && nuc_it->hl <= part->value[4])
+		  else if (it->hl > part->value[3] && it->hl <= part->value[4])
 		    {
-		      nuc_it->colour = part->colour[4];
+		      it->colour = part->colour[4];
 		      part->draw[4]=true;
 		    }
-		  else if (nuc_it->hl > part->value[4] && nuc_it->hl <= part->value[5])
+		  else if (it->hl > part->value[4] && it->hl <= part->value[5])
 		    {
-		      nuc_it->colour = part->colour[5];
+		      it->colour = part->colour[5];
 		      part->draw[5]=true;
 		    }
-		  else if (nuc_it->hl > part->value[5] && nuc_it->hl <= part->value[6])
+		  else if (it->hl > part->value[5] && it->hl <= part->value[6])
 		    {
-		      nuc_it->colour = part->colour[6];
+		      it->colour = part->colour[6];
 		      part->draw[6]=true;
 		    }
 		  else
 		    {
-		      nuc_it->colour = part->colour[7];
+		      it->colour = part->colour[7];
 		      part->draw[7]=true;
 		    }
 		}
 	      else
-		nuc_it->show=0;
+		it->show=0;
 	    }
 	  // 1st isomer energy
 	  else if (draw->choice == "e")
 	    {
-	      if (nuc_it->st == 1)
+	      if (it->st == 1)
 		{
-		  nuc_it->show=1;
+		  it->show=1;
 
-		  if (nuc_it->is_nrg <= part->value[0])
+		  if (it->is_nrg <= part->value[0])
 		    {
-		      nuc_it->colour = part->colour[0];
+		      it->colour = part->colour[0];
 		      part->draw[0]=true;
 		    }
-		  else if (nuc_it->is_nrg > part->value[0] && nuc_it->is_nrg <= part->value[1])
+		  else if (it->is_nrg > part->value[0] && it->is_nrg <= part->value[1])
 		    {
-		      nuc_it->colour = part->colour[1];
+		      it->colour = part->colour[1];
 		      part->draw[1]=true;
 		    }
-		  else if (nuc_it->is_nrg > part->value[1] && nuc_it->is_nrg <= part->value[2])
+		  else if (it->is_nrg > part->value[1] && it->is_nrg <= part->value[2])
 		    {
-		      nuc_it->colour = part->colour[2];
+		      it->colour = part->colour[2];
 		      part->draw[2]=true;
 		    }
-		  else if (nuc_it->is_nrg > part->value[2] && nuc_it->is_nrg <= part->value[3])
+		  else if (it->is_nrg > part->value[2] && it->is_nrg <= part->value[3])
 		    {
-		      nuc_it->colour = part->colour[3];
+		      it->colour = part->colour[3];
 		      part->draw[3]=true;
 		    }
-		  else if (nuc_it->is_nrg > part->value[3] && nuc_it->is_nrg <= part->value[4])
+		  else if (it->is_nrg > part->value[3] && it->is_nrg <= part->value[4])
 		    {
-		      nuc_it->colour = part->colour[4];
+		      it->colour = part->colour[4];
 		      part->draw[4]=true;
 		    }
 		  else
 		    {
-		      nuc_it->colour = part->colour[5];
+		      it->colour = part->colour[5];
 		      part->draw[5]=true;
 		    }
 		}
-	      else if (nuc_it->st == 0 && (nuc_it+1)->st != 1)
+	      else if (it->st == 0 && (it+1)->st != 1)
 		{
 		  // As not every nucleus has an isomer, draw empty boxes as a visual aid
-		  nuc_it->colour = "white";
+		  it->colour = "white";
 		  part->draw[6]=true;
-		  nuc_it->show=2;
+		  it->show=2;
 		}
 	    }
 	}

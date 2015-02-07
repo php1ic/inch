php1ic/inch

commit 70bb1f89d545c3dcd25272c87f1174a0a8b516b5

Add comments to explain the extraction of the decay mode.
Also change vector type from int->bool as we are only using it
for a yes/no check.

src/nuclide.cpp
@@ -385,7 +385,7 @@ void Nuclide::setHalfLife()
 }
 
 
-void Nuclide::setDecayMode(std::vector<int> &pnSide)
+void Nuclide::setDecayMode(std::vector<bool> &pnSide)
 {
   // Store how ground-state decays in member decay
   if (st == 0)
@@ -395,23 +395,41 @@ void Nuclide::setDecayMode(std::vector<int> &pnSide)
       if (full_data.size() >= 106)
 	Decay = full_data.substr(106);
 
-      if (Decay.find(";")  != std::string::npos) Decay.erase(Decay.find(";"));
-      if (Decay.find("=")  != std::string::npos) Decay.erase(Decay.find("="));
-      if (Decay.find("~")  != std::string::npos) Decay.erase(Decay.find("~"));
-      if (Decay.find(">")  != std::string::npos) Decay.erase(Decay.find(">"));
-      if (Decay.find("<")  != std::string::npos) Decay.erase(Decay.find("<"));
-      if (Decay.find(" ?") != std::string::npos) Decay = "unknown";
-      if (Decay.find(" ")  != std::string::npos) Decay.erase(Decay.find(" "));
+      // If more than 1 decay mode, they are separated by a ';'
+      // Currently only want the 1st mode.
+      if (Decay.find(";")  != std::string::npos)
+	Decay.erase(Decay.find(";"));
 
-      // Book keeping, swap e+ for B+, and use "stable" instead of "IS"
+      // Chop out everything after the '='
+      if (Decay.find("=")  != std::string::npos)
+	Decay.erase(Decay.find("="));
+      // Or convert a guess/estimate to unknown
+      else if (Decay.find(" ?") != std::string::npos)
+	Decay = "unknown";
+
+      // Remove any remaining unwanted characters from what
+      // is left of the string.
+      std::string unwantedCharacters("~<> ");
+      size_t found = Decay.find_first_of(unwantedCharacters);
+
+      while (found!=std::string::npos)
+	{
+	  Decay.erase(found,1);
+	  found=Decay.find_first_of(unwantedCharacters,found+1);
+	}
+
+      // Book keeping
+      // swap e+ for B+
       if ( Decay == "e+")
 	Decay = "B+";
-
-      if ( Decay == "IS")
+      // use "stable" instead of "IS"
+      else if ( Decay == "IS")
 	{
 	  Decay = "stable";
 
-	  if ( !pnSide[Z] )
+	  // In the isotopic chain, mark the N value of the
+	  // first stable isotope.
+	  if ( pnSide[Z] == false )
 	    pnSide[Z] = 1;
 	}
 
@@ -422,9 +440,9 @@ void Nuclide::setDecayMode(std::vector<int> &pnSide)
 }
 
 
-void Nuclide::setNeutronOrProtonRich(std::vector<int> &pnSide)
+void Nuclide::setNeutronOrProtonRich(std::vector<bool> &pnSide)
 {
-  if ( !pnSide[Z] )
+  if ( pnSide[Z] == false )
     rich = 2;
   else
     rich = (decay == "stable") ? 6 : 3;

include/nuclide.h
@@ -72,8 +72,8 @@ public:
   void setSeparationEnergies(std::vector<Nuclide> &);
   void setIsomerEnergy();
   void setHalfLife();
-  void setDecayMode(std::vector<int> &);
-  void setNeutronOrProtonRich(std::vector<int> &);
+  void setDecayMode(std::vector<bool> &);
+  void setNeutronOrProtonRich(std::vector<bool> &);
 
 };
 

src/readNUBASE.cpp
@@ -29,7 +29,7 @@ bool readNUBASE(const std::string &table,
   // Reserving space avoids multiple calls to the copy constructor
   nuc.reserve(countLinesInFile(file));
 
-  std::vector<int> pnSide(MAX_Z+1,0);
+  std::vector<bool> pnSide(MAX_Z+1,0);
   std::string line;
 
   while (getline(file,line))

commit 847dd79af3cd12987809851c815f55b1956bb766

Commit 70bb1f89d introduced a bug that broke decay mode displaying. Fixed
Only the 'bad' character was removed from the string, but we should
have been deleting form the character to the end of the string as
the part we are interested in is before this. I really need to write
some tests!

src/nuclide.cpp
@@ -1,5 +1,7 @@
 #include "include/nuclide.h"
 
+#include <iostream>
+
 Nuclide::Nuclide(std::string line):
   //bool
   own(false),
@@ -401,8 +403,8 @@ void Nuclide::setDecayMode(std::vector<bool> &pnSide)
 
       while (found!=std::string::npos)
 	{
-	  Decay.erase(found,1);
-	  found=Decay.find_first_of(unwantedCharacters,found+1);
+          Decay.erase(found);
+          found = Decay.find_first_of(unwantedCharacters,found+1);
 	}
 
       // Book keeping
e4a465d32574bd6919ab763537c3ef17b35a7634
Ian Cullen <ian.james.cullen@gmail.com>
2015-03-07 14:26:15 +0000

    Untabbify and remove comment that is no longer accurate.

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,10 @@
-///////////////////////////////////////////////////////////////
-//                                                           //
-// inch                                                      //
-//                                                           //
-///////////////////////////////////////////////////////////////
-//                                                           //
-// Description: This program will create a nuclear chart     //
-//              containing the nuclei specified by the user, //
-//              coloured by a user chosen property           //
-//                                                           //
-// Input:       Required - None                              //
-//                                                           //
-//              Optional - The name of the output file       //
-//                       - An input file containing values   //
-//                         used to create the chart          //
-//                                                           //
-//                                                           //
-// Output:      An encapsulated postscript (eps) file        //
-//              or a scalable vector graphics (svg) file     //
-//              or a latex figure file (tikz) file           //
-//                                                           //
-///////////////////////////////////////////////////////////////
-
 #include "include/createChart.h"
 
 int main(int argc, char *argv[])
 {
   inputs *draw = new inputs;
 
-  if (argc == 2
+  if ( argc == 2
       && (argv[1] == std::string("--version") || argv[1] == std::string("-v"))
       )
     {
@@ -42,7 +19,7 @@ int main(int argc, char *argv[])
   draw->showBanner();
   draw->constructFullyQualifiedPaths();
 
-  if (argc > 1)
+  if ( argc > 1 )
     {
       std::vector<std::string> arguments(argv,argv+argc);
       createChart *chart = new createChart(draw,part,nuc,arguments);
@@ -57,8 +34,8 @@ int main(int argc, char *argv[])
   draw->writeOptionFile();
 
   std::cout << "Enjoy\n"
-	    << "\nTo run again with the same options: " << argv[0]
-	    << " -i options.in\n" << std::endl;
+            << "\nTo run again with the same options: " << argv[0]
+            << " -i options.in\n" << std::endl;
 
   delete draw;
   delete part;

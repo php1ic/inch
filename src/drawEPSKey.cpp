88f40677dbe5dbad387017eed878ed681980c158
Ian Cullen <ian.james.cullen@gmail.com>
2015-06-21 22:50:51 +0100

    Untabbify and add comments about the 'magic numbers'.
    Should make consts out of the above numbers or work out how to not
    use them.

--- src/drawEPSKey.cpp
+++ src/drawEPSKey.cpp
@@ -1,31 +1,31 @@
 #include "include/functions.h"
 
 void drawEPSKey(const inputs *draw,
-		std::ofstream &outFile,
-		const partition *part
-		)
+                std::ofstream &outFile,
+                const partition *part
+                )
 {
   std::vector<std::string> keyString(part->draw.size());
 
   std::cout << "Drawing the key ";
 
   outFile << "\n%-------\n"
-	  << "%- Key -\n"
-	  << "%-------\n"
-	  << "/ResetWidth {/StringLength 0 def} def\n"
-	  << "/TotalWidth {dup stringwidth pop StringLength add /StringLength exch def} def\n"
-	  << "/KeyWidth 0 def\n"
-	  << "/TestWidth {StringLength KeyWidth gt {/KeyWidth StringLength def} if} def\n\n"
-	  << "%-lower left corner of the key-\n";
+          << "%- Key -\n"
+          << "%-------\n"
+          << "/ResetWidth {/StringLength 0 def} def\n"
+          << "/TotalWidth {dup stringwidth pop StringLength add /StringLength exch def} def\n"
+          << "/KeyWidth 0 def\n"
+          << "/TestWidth {StringLength KeyWidth gt {/KeyWidth StringLength def} if} def\n\n"
+          << "%-lower left corner of the key-\n";
 
   if (draw->section == "a")
     {
       int fullChartKeyPosition[5][2]={
-	{15,75},
-	{12,81},
-	{9,60},
-	{9,75},
-	{15,75}
+        {15,75},
+        {12,81},
+        {9,60},
+        {9,75},
+        {15,75}
       };
 
       int index=0;
@@ -37,12 +37,14 @@ void drawEPSKey(const inputs *draw,
       else if (draw->choice=="e") index=4;
 
       outFile << fullChartKeyPosition[index][0] << " "
-	      << fullChartKeyPosition[index][1] << " translate\n";
+              << fullChartKeyPosition[index][1] << " translate\n";
     }
+  //The value of 9 is aesthetic and is the border between vertically centering the key,
+  //or vertically centering the chart.
   else if (draw->Zmax-draw->Zmin >= 9)
     {
       outFile << (draw->Nmax-draw->Nmin+2) << " "
-	      << 0.5*((draw->Zmax-draw->Zmin+1.0)-draw->key_height*draw->key_scale) << " translate\n";
+              << 0.5*( (draw->Zmax-draw->Zmin+1.0) - draw->key_height*draw->key_scale ) << " translate\n";
     }
   else
     {
@@ -54,28 +56,28 @@ void drawEPSKey(const inputs *draw,
   if (draw->choice == "b")
     {
       outFile << "\n/exponent{\n"
-	      << "/e1 ed\n"
-	      << "/e2 ed\n"
-	      << "1 TR e2 2 string cvs TotalWidth sh\n"
-	      << "0.75 TR (x) TotalWidth sh\n"
-	      << "1 TR (10) TotalWidth sh\n"
-	      << "gs\n"
-	      << "0.75 TR\n"
-	      << "0 0.4 rmoveto e1 2 string cvs TotalWidth sh\n"
-	      << "gr\n"
-	      << "} def\n\n"
-	      << "/printUnit{\n"
-	      << "1 TR (   < ) TotalWidth sh\n"
-	      << "1 S (d) TotalWidth sh\n"
-	      << "1 TR (m/m < ) TotalWidth sh\n"
-	      << "} def\n" << std::endl;
+              << "/e1 ed\n"
+              << "/e2 ed\n"
+              << "1 TR e2 2 string cvs TotalWidth sh\n"
+              << "0.75 TR (x) TotalWidth sh\n"
+              << "1 TR (10) TotalWidth sh\n"
+              << "gs\n"
+              << "0.75 TR\n"
+              << "0 0.4 rmoveto e1 2 string cvs TotalWidth sh\n"
+              << "gr\n"
+              << "} def\n\n"
+              << "/printUnit{\n"
+              << "1 TR (   < ) TotalWidth sh\n"
+              << "1 S (d) TotalWidth sh\n"
+              << "1 TR (m/m < ) TotalWidth sh\n"
+              << "} def\n" << std::endl;
     }
   else if (draw->choice == "d")
     {
       outFile << "\n/printUnit{gs\n"
-	      << "1 S (t) sh\n"
-	      << "0.5 TR 0 -0.15 rmoveto (1/2) sh\n"
-	      << "gr} def\n" << std::endl;
+              << "1 S (t) sh\n"
+              << "0.5 TR 0 -0.15 rmoveto (1/2) sh\n"
+              << "gr} def\n" << std::endl;
     }
 
   //-Construct an array of strings with the text for each part of the key
@@ -85,25 +87,26 @@ void drawEPSKey(const inputs *draw,
   for (size_t i=0; i<part->draw.size(); ++i)
     {
       if (part->draw[part->draw.size()-(i+1)])
-  	{
-	  outFile << "0 " << part->colour[11-i] << " 0.5 " << relativeKeyPosition << " curve Nucleus\n"
-		  << "2.5 " << relativeKeyPosition+0.2 << " m ResetWidth\n"
-		  << keyString[11-i];
+        {
+          //The max size of part->colour is 12
+          outFile << "0 " << part->colour[11-i] << " 0.5 " << relativeKeyPosition << " curve Nucleus\n"
+                  << "2.5 " << relativeKeyPosition+0.2 << " m ResetWidth\n"
+                  << keyString[11-i];
 
-	  relativeKeyPosition+=1.5;
-  	}
+          relativeKeyPosition+=1.5;
+        }
     }
 
   //-Draw a dynamically sized box around the key
   outFile << "\n"
-	  << "%Draw a box around the key\n"
-	  << "0.1 u div sl\n"
-	  << "0 0 m\n"
-	  << "KeyWidth 3 add 0 rl\n"
-	  << "0 " << draw->key_height << " rl\n"
-	  << "KeyWidth 3 add neg 0 rl\n"
-	  << "closepath\n"
-	  << "st\n" << std::endl;
+          << "%Draw a box around the key\n"
+          << "0.1 u div sl\n"
+          << "0 0 m\n"
+          << "KeyWidth 3 add 0 rl\n"
+          << "0 " << draw->key_height << " rl\n"
+          << "KeyWidth 3 add neg 0 rl\n"
+          << "closepath\n"
+          << "st\n" << std::endl;
 
   std::cout << "- done" << std::endl;
 }

48b38e756838eac70b7ce2a81c5f3531ba0928b2
Ian Cullen <ian.james.cullen@gmail.com>
2016-03-10 21:34:42 +0000

    Fix error message and untabbify.

--- src/readOWN.cpp
+++ src/readOWN.cpp
@@ -1,17 +1,17 @@
 #include "include/functions.h"
 
 bool readOWN(const std::string &myNuclei,
-	     std::vector<Nuclide> &nuc
-	     )
+             std::vector<Nuclide> &nuc
+             )
 {
   std::cout << "Reading " << myNuclei.substr(myNuclei.find_last_of("/")+1)
-	    << " for user selected nuclei (--";
+            << " for user selected nuclei (--";
 
   if ( !checkFileExists(myNuclei) )
     {
       std::cout << "\n"
-		<< "***ERROR***: File " << myNuclei
-		<< " couldn't be opened." << std::endl;
+                << "***ERROR***: File " << myNuclei
+                << " does not exist." << std::endl;
       return false;
     }
 
@@ -20,17 +20,17 @@ bool readOWN(const std::string &myNuclei,
   if ( !inFile.is_open() )
     {
       std::cout << "\n"
-		<< "***ERROR***: " <<  myNuclei
-		<< " couldn't be opened, does it exist?\n" << std::endl;
+                << "***ERROR***: " <<  myNuclei
+                << " couldn't be opened.\n" << std::endl;
       return false;
     }
 
   std::string line;
 
-  while (getline(inFile,line))
+  while ( getline(inFile,line) )
     {
       if ( !line.compare("") || line.at(0) == '#' )
-	continue;
+        continue;
 
       int N=0;
       int Z=0;
@@ -38,17 +38,17 @@ bool readOWN(const std::string &myNuclei,
 
       sscanf(line.c_str(), "%d %d %d", &N, &Z, &st);
 
-      for (std::vector<Nuclide>::iterator it=nuc.begin(); it!=nuc.end(); ++it)
-	{
-	  if(   it->N  == N
-	     && it->Z  == Z
-	     && it->st == st
-	     )
-	    {
-	      it->setOwn(true);
-	      break;
-	    }
-	}
+      for ( std::vector<Nuclide>::iterator it=nuc.begin(); it!=nuc.end(); ++it )
+        {
+          if(   it->N  == N
+             && it->Z  == Z
+             && it->st == st
+             )
+            {
+              it->setOwn(true);
+              break;
+            }
+        }
     }
 
   inFile.close();

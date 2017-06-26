e79b2bb6857ac2c409f0b99413c088bc48f2aced
Ian Cullen <ian.james.cullen@gmail.com>
2017-06-26 15:08:32 +0100

    Check that code analysis tool exists and add comments.

--- scripts/code_check.sh
+++ scripts/code_check.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-rootdir=$(git rev-parse --show-toplevel)
+#Set the analyser to use
+CODECHECKER=cppcheck
 
+#Check it's actually installed
+if ! command -v ${CODECHECKER} >/dev/null 2>&1
+then
+    echo -e "\n\t***ERROR***: Code check tool is set to ${CODECHECKER} but it isn't installed\n"
+    exit 1
+fi
+
+#Use absolute paths
+rootdir="$(git rev-parse --show-toplevel)"
 sources="${rootdir}/src"
 includes="${rootdir}/include"
 
-cppcheck \
+#Run the analysis
+${CODECHECKER} \
     --enable=all \
     --suppress=missingIncludeSystem \
     -I"${includes}" \

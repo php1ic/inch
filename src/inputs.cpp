php1ic/inch

commit d0c2cb314bd4214297f8d4d8d9d2f39ad9d5e568

Add references to the new mass tables, keep 2003 as default

src/inputs.cpp
@@ -41,6 +41,7 @@ inputs::inputs():
   single_drip_lines(1),                // 0=none, 1=both, 2=p-only, 3=n-only
   double_drip_lines(1),
   file_type(0),                        // 0=eps, 1=svg, 2=tikz
+  year(3),                              // 3, 12, 16
   //double
   curve(0.25),
   key_height(0.5),
@@ -50,8 +51,6 @@ inputs::inputs():
   //std::string
   path("./"),
   mass_table(""),
-  mass_table_AME("mass.mas114"),
-  mass_table_NUBASE("nubtab03.asc"),
   my_nuclei("my_nuclei.dat"),
   r_proc_path("r-process.dat"),
   neutron_drip("neutron.drip"),
@@ -144,6 +143,30 @@ void inputs::constructFullyQualifiedPaths()
   pwd.append("/");
 
   options.insert(0,pwd);
+}
+
+
+void inputs::setTableYear(const int _year)
+{
+  switch (_year)
+    {
+    default:
+    case 3:
+      year = 3;
+      mass_table_NUBASE = "nubtab03.asc";
+      mass_table_AME    = "mass.mas114";
+      break;
+    case 12:
+      year = _year;
+      mass_table_NUBASE = "nubase.mas12";
+      mass_table_AME    = "mass.mas12";
+      break;
+    case 16:
+      year = _year;
+      mass_table_NUBASE = "nubase2016.txt";
+      mass_table_AME    = "mass16.txt";
+      break;
+    }
 }
 
 

include/inputs.h
@@ -28,6 +28,7 @@ public:
   void constructOutputFilename();
   void setCanvasSize();
   void setExtreme(const std::string &limit);
+  void setTableYear(const int year);
   void showBanner() const;
   void showChartOptions() const;
   void showVersion() const;
@@ -55,6 +56,7 @@ public:
   int single_drip_lines;
   int double_drip_lines;
   int file_type;
+  int year;
 
   double curve;
   double key_height;

commit ef0a70494e816a1be30d9cdf458baba31e49e026

Mass tables are read prior to console arguments, need the default values
Revert a bug introduced in d0c2cb314bd, looks like setting the year of
table to use via a command line option may be non-trivial.

src/inputs.cpp
@@ -51,6 +51,8 @@ inputs::inputs():
   //std::string
   path("./"),
   mass_table(""),
+  mass_table_AME("mass.mas114"),
+  mass_table_NUBASE("nubtab03.asc"),
   my_nuclei("my_nuclei.dat"),
   r_proc_path("r-process.dat"),
   neutron_drip("neutron.drip"),
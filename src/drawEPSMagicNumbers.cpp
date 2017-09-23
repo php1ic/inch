aeb9780389867f3c6da4b8760128950ffceab33b
Ian Cullen <ian.james.cullen@gmail.com>
2017-09-23 21:59:17 +0100

    Remove magic number by using a 2D vector instead of array

--- src/drawEPSMagicNumbers.cpp
+++ src/drawEPSMagicNumbers.cpp
@@ -6,28 +6,26 @@ void drawEPSMagicNumbers(const std::unique_ptr<inputs> &draw,
 {
   std::cout << "Drawing magic numbers";
 
-  const int MAGIC_NUMBERS = 7;
-
   //0 - Magic number
   //1 - min n(x) value for magic number line
   //2 - max n(x)
   //3 - min z(y)
   //4 - max z(y)
-  double EndPoints[MAGIC_NUMBERS][5] = {
-    {  2.0,  0.0,  21.5,  0.0, 11.5},
-    {  8.0,  1.0,  29.5,  0.0, 21.5},
-    { 20.0,  7.5,  51.5,  1.5, 32.5},
-    { 28.0, 16.5,  55.5,  7.5, 37.5},
-    { 50.0, 45.5,  91.5, 19.5, 55.5},
-    { 82.0, 81.5, 139.5, 43.5, 83.5},
-    {126.0,  0.0,   0.0, 76.5, 94.5}, // Z<=118 so Z=126 magic number is not needed
-  };
+  std::vector< std::vector<double> > EndPoints =
+    {
+      {  2.0,  0.0,  21.5,  0.0, 11.5},
+      {  8.0,  1.0,  29.5,  0.0, 21.5},
+      { 20.0,  7.5,  51.5,  1.5, 32.5},
+      { 28.0, 16.5,  55.5,  7.5, 37.5},
+      { 50.0, 45.5,  91.5, 19.5, 55.5},
+      { 82.0, 81.5, 139.5, 43.5, 83.5},
+      {126.0,  0.0,   0.0, 76.5, 94.5}, // Z<=118 so Z=126 magic number is not needed
+    };
 
-
-  double max  = 0.0;
-  double min  = 0.0;
-  double low  =-0.25;
-  double high = 1.25;
+  double max  =  0.0;
+  double min  =  0.0;
+  double low  = -0.25;
+  double high =  1.25;
 
   outFile << "\n%-----------------\n"
           << "%- Magic Numbers -\n"
@@ -36,7 +34,7 @@ void drawEPSMagicNumbers(const std::unique_ptr<inputs> &draw,
           << "black rgb\n"
           << "1 u div sl" << std::endl;
 
-  for ( int i=0; i<MAGIC_NUMBERS; ++i )
+  for ( size_t i=0; i<EndPoints.size(); ++i )
     {
       //-Proton
       if (   draw->Zmax >= EndPoints[i][0]

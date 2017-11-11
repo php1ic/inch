ca56a49d1bd606087dd16e1edf45df549336ceac
Ian Cullen <ian.james.cullen@gmail.com>
2017-11-11 20:24:10 +0000

    Didn't know you could initialise in the header. Lets do that

--- include/inputs.h
+++ include/inputs.h
@@ -52,55 +52,71 @@ public:
 
   FileType filetype;
 
-  bool grid;
-  bool magic_numbers;
-  bool write_isotope;
-  bool r_process;
-  bool key;
-  bool own_nuclei;
-  bool AME;
-  bool key_relative;
-  bool valid_inputfile;
+  bool grid = false;
+  bool magic_numbers = true;
+  bool write_isotope = true;
+  bool r_process = true;
+  bool key = true;
+  bool own_nuclei = true;
+  bool AME = false;
+  bool key_relative = true;
+  bool valid_inputfile = false;
 
-  int valid_console;
-  int Zmin;
-  int Zmax;
-  int Nmin;
-  int Nmax;
-  int size;
-  int experimental;
-  int np_rich;
-  int single_drip_lines;
-  int double_drip_lines;
-  int year;
+  int valid_console = 1;
+  int Zmin = MAX_Z;
+  int Zmax = MIN_Z;
+  int Nmin = MAX_N;
+  int Nmax = MIN_N;
+  int experimental = 0;
+  // 1=all, 2=p-rich and stable, 3=n-rich and stable, 6=stable only
+  int np_rich = 1;
+  // 0=none, 1=both, 2=p-only, 3=n-only
+  int single_drip_lines = 1;
+  int double_drip_lines = 1;
+  //3,12,16
+  int year = 3;
+  //-----------------------------------------------------------------------
+  //- If the file is to be converted into some other format (eg.jpg,png), -
+  //- without any resizing, and the whole chart is drawn. The following  --
+  //- values are the largest allowed for the given page size. -------------
+  //-----------------------------------------------------------------------
+  // A0 -> size = 18
+  // A1 -> size = 13
+  // A2 -> size =  9
+  // A3 -> size =  6
+  // A4 -> size =  4
+  // A5 -> size =  3
+  //-----------------
+  int size = 4;
 
-  double curve;
-  double key_height;
-  double key_scale;
-  double chart_height;
-  double chart_width;
+  double curve = 0.25;
+  double key_height = 0.5;
+  double key_scale = 0.0;
+  double chart_height = 0.0;
+  double chart_width = 0.0;
 
-  std::string path;
-  std::string mass_table;
-  std::string mass_table_AME;
-  std::string mass_table_NUBASE;
-  std::string my_nuclei;
-  std::string r_proc_path;
-  std::string neutron_drip;
-  std::string proton_drip;
-  std::string two_neutron_drip;
-  std::string two_proton_drip;
+  std::string path = "./";
+  std::string mass_table = "";
+  std::string mass_table_AME = "mass.mas114";
+  std::string mass_table_NUBASE = "nubtab03.asc";
+  std::string my_nuclei = "my_nuclei.dat";
+  std::string r_proc_path = "r-process.dat";
+  std::string neutron_drip = "neutron.drip";
+  std::string proton_drip = "proton.drip";
+  std::string two_neutron_drip = "2neutron.drip";
+  std::string two_proton_drip = "2proton.drip";
+  std::string options = "options.in";
+  // Without extension, this is added in the code
+  std::string outfile = "chart";
+  std::string FRDM = "FRLDM_ME.tbl";
+  std::string version = "0.9.7";
+  std::string pwd;
   std::string stringfile_type;
   std::string choice;
   std::string required;
   std::string section;
   std::string type;
-  std::string options;
   std::string inputfile;
-  std::string outfile;
-  std::string FRDM;
-  std::string version;
-  std::string pwd;
 
   std::vector<std::pair<int,int> > r_process_data;
   std::map<std::string, std::string> arguments;
--- src/inputs.cpp
+++ src/inputs.cpp
@@ -7,70 +7,7 @@ inputs::convertZToSymbol() definitions are in their own files.
 inputs::convertSymbolToZ() definitions are in their own files.
  */
 
-
-//-----------------------------------------------------------------------
-//- If the file is to be converted into some other format (eg.jpg,png), -
-//- without any resizing, and the whole chart is drawn. The following  --
-//- values are the largest allowed for the given page size. -------------
-//-----------------------------------------------------------------------
-// A0 -> size = 18
-// A1 -> size = 13
-// A2 -> size =  9
-// A3 -> size =  6
-// A4 -> size =  4
-// A5 -> size =  3
-//-----------------
-
-inputs::inputs():
-  //bool
-  grid(false),
-  magic_numbers(true),
-  write_isotope(true),
-  r_process(true),
-  key(true),
-  own_nuclei(true),
-  AME(false),
-  key_relative(true),                  // Not an option, just default value
-  valid_inputfile(false),
-  //int
-  valid_console(1),
-  Zmin(MAX_Z),
-  Zmax(MIN_Z),
-  Nmin(MAX_N),
-  Nmax(MIN_N),
-  size(4),                             // See comment above
-  experimental(0),
-  np_rich(1),                          // 1=all, 2=p-rich and stable, 3=n-rich and stable, 6=stable only
-  single_drip_lines(1),                // 0=none, 1=both, 2=p-only, 3=n-only
-  double_drip_lines(1),
-  year(3),                             // 3, 12, 16
-  //double
-  curve(0.25),
-  key_height(0.5),
-  key_scale(0.0),
-  chart_height(0.0),
-  chart_width(0.0),
-  //std::string
-  path("./"),
-  mass_table(""),
-  mass_table_AME("mass.mas114"),
-  mass_table_NUBASE("nubtab03.asc"),
-  my_nuclei("my_nuclei.dat"),
-  r_proc_path("r-process.dat"),
-  neutron_drip("neutron.drip"),
-  proton_drip("proton.drip"),
-  two_neutron_drip("2neutron.drip"),
-  two_proton_drip("2proton.drip"),
-  choice(""),
-  required(""),
-  section(""),
-  type(""),
-  options("options.in"),
-  inputfile(""),
-  outfile("chart"),                    // Without extension, this is added in the code
-  FRDM("FRLDM_ME.tbl"),
-  version("0.9.7"),
-  pwd("")
+inputs::inputs()
 {
   setFileType("eps");
   constructFullyQualifiedPaths();

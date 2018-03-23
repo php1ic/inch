05ee8951e93863de3c5b0495420130ec3d628db3
Ian Cullen <ian.james.cullen@gmail.com>
2018-03-23 18:37:09 +0000

    Lessen scope and traverse vector with iterators

--- src/createDriplineFile.cpp
+++ src/createDriplineFile.cpp
@@ -1,21 +1,5 @@
 #include "functions.hpp"
 
-// Rather than use a vector of Nuclide(216), create a new
-// smaller(56) struct to store the required values and
-// create a vector of these.
-struct isotope
-{
-  int A;
-  int Z;
-  int N;
-  double s_n;
-  double s_2n;
-  double s_p;
-  double s_2p;
-  double ME;
-};
-
-
 void createDriplineFile(const std::unique_ptr<inputs> &draw,
                         const double meN,
                         const double meP,
@@ -116,13 +100,26 @@ void createDriplineFile(const std::unique_ptr<inputs> &draw,
   int previousDoubleNeutron = 8;
   int previousDoubleProton  = 0;
 
-  int i=0;
-  int nucleonWidth=4;
-  int dripWidth=8;
-  std::string line;
+  int nucleonWidth = 4;
+  int dripWidth = 8;
 
+  // Rather than use a vector of Nuclide(216), create a new
+  // smaller(56) struct to store the required values and
+  // create a vector of these.
+  struct isotope
+  {
+    int A = 0;
+    int Z = 0;
+    int N = 0;
+    double s_n = 0.0;
+    double s_2n = 0.0;
+    double s_p = 0.0;
+    double s_2p = 0.0;
+    double ME = 0.0;
+  };
   std::vector<isotope> dripNuc;
 
+  std::string line;
   while( getline(file,line) )
     {
       if ( line.empty() || line[0] == '#' )
@@ -134,125 +131,124 @@ void createDriplineFile(const std::unique_ptr<inputs> &draw,
       std::string dummy;
 
       dripNuc.emplace_back(isotope());
+      auto currentIsotope = std::rbegin(dripNuc);
 
-      dripData >> dripNuc[i].A >> dripNuc[i].Z >> dummy >> dripNuc[i].ME;
+      dripData >> currentIsotope->A >> currentIsotope->Z >> dummy >> currentIsotope->ME;
 
-      dripNuc[i].N = dripNuc[i].A - dripNuc[i].Z;
+      currentIsotope->N = currentIsotope->A - currentIsotope->Z;
 
       /// File is ordered in Z then A, by looping through backwards,
       /// we do not need to traverse the entire contents as we can stop
       /// once values get too far apart to provide data.
-      for ( int j=i; j>0; --j )
+      for ( auto otherIsotope = currentIsotope; otherIsotope != std::rend(dripNuc); ++otherIsotope )
         {
           /// Depending on the drip line, we can break when differences
           /// in N or Z get too larger i.e. 2 for single particle and
           /// 3 for double particle lines.
-          if (   (np == 0 && dripNuc[i].N - dripNuc[j].N >= 2)
-              || (np == 1 && dripNuc[i].N - dripNuc[j].N >= 3)
-              || (np == 2 && dripNuc[i].Z - dripNuc[j].Z >= 2)
-              || (np == 3 && dripNuc[i].Z - dripNuc[j].Z >= 3)
+          if (   (np == 0 && currentIsotope->N - otherIsotope->N >= 2)
+              || (np == 1 && currentIsotope->N - otherIsotope->N >= 3)
+              || (np == 2 && currentIsotope->Z - otherIsotope->Z >= 2)
+              || (np == 3 && currentIsotope->Z - otherIsotope->Z >= 3)
               )
             {
               break;
             }
 
-          if ( dripNuc[i].A - dripNuc[j].A == 1 )
+          if ( currentIsotope->A - otherIsotope->A == 1 )
             {
               /// S(n) = M(Z,N-1) - M(Z,N) + M(0,1)
-              if ( np == 0 && dripNuc[j].Z == dripNuc[i].Z && currentSingleProton < dripNuc[i].Z )
+              if ( np == 0 && otherIsotope->Z == currentIsotope->Z && currentSingleProton < currentIsotope->Z )
                 {
-                  dripNuc[i].s_n = dripNuc[j].ME - dripNuc[i].ME + meN;
+                  currentIsotope->s_n = otherIsotope->ME - currentIsotope->ME + meN;
 
-                  if ( dripNuc[i].s_n < 0.0 )
+                  if ( currentIsotope->s_n < 0.0 )
                     {
                       dripFile << std::fixed
-                               << std::setw(nucleonWidth) << dripNuc[i].N   << " "
-                               << std::setw(nucleonWidth) << dripNuc[i].Z   << " "
-                               << std::setw(dripWidth) << dripNuc[i].s_n << "\n"
-                               << std::setw(nucleonWidth) << dripNuc[i].N   << " "
-                               << std::setw(nucleonWidth) << dripNuc[i].Z+1 << " "
-                               << std::setw(dripWidth) << dripNuc[i].s_n << std::endl;
+                               << std::setw(nucleonWidth) << currentIsotope->N   << " "
+                               << std::setw(nucleonWidth) << currentIsotope->Z   << " "
+                               << std::setw(dripWidth) << currentIsotope->s_n << "\n"
+                               << std::setw(nucleonWidth) << currentIsotope->N   << " "
+                               << std::setw(nucleonWidth) << currentIsotope->Z+1 << " "
+                               << std::setw(dripWidth) << currentIsotope->s_n << std::endl;
 
                       ++currentSingleProton;
                     }
                 }
 
               /// S(p) = M(Z-1,N) - M(Z,N) + M(1,0)
-              if ( np == 2 && dripNuc[j].N == dripNuc[i].N && currentSingleNeutron < dripNuc[i].N )
+              if ( np == 2 && otherIsotope->N == currentIsotope->N && currentSingleNeutron < currentIsotope->N )
                 {
-                  dripNuc[i].s_p = dripNuc[j].ME - dripNuc[i].ME + meP;
+                  currentIsotope->s_p = otherIsotope->ME - currentIsotope->ME + meP;
 
-                  if ( dripNuc[i].s_p < 0.0 )
+                  if ( currentIsotope->s_p < 0.0 )
                     {
-                      if ( dripNuc[i].N != previousSingleNeutron )
+                      if ( currentIsotope->N != previousSingleNeutron )
                         {
                           dripFile << std::fixed
                                    << std::setw(nucleonWidth) << previousSingleNeutron+1 << " "
                                    << std::setw(nucleonWidth) << previousSingleProton    << " "
-                                   << std::setw(dripWidth) << dripNuc[i].s_p          << std::endl;
+                                   << std::setw(dripWidth) << currentIsotope->s_p          << std::endl;
                         }
 
                       dripFile << std::fixed
-                               << std::setw(nucleonWidth) << dripNuc[i].N   << " "
-                               << std::setw(nucleonWidth) << dripNuc[i].Z   << " "
-                               << std::setw(dripWidth) << dripNuc[i].s_p << std::endl;
+                               << std::setw(nucleonWidth) << currentIsotope->N   << " "
+                               << std::setw(nucleonWidth) << currentIsotope->Z   << " "
+                               << std::setw(dripWidth) << currentIsotope->s_p << std::endl;
 
                       ++currentSingleNeutron;
-                      previousSingleProton  = dripNuc[i].Z;
-                      previousSingleNeutron = dripNuc[i].N;
+                      previousSingleProton  = currentIsotope->Z;
+                      previousSingleNeutron = currentIsotope->N;
                     }
                 }
             }
-          else if ( dripNuc[i].A - dripNuc[j].A == 2 )
+          else if ( currentIsotope->A - otherIsotope->A == 2 )
             {
               /// S(2n) = M(Z,N-2) - M(Z,N) + 2*M(0,1)
-              if ( np == 1 && dripNuc[j].Z == dripNuc[i].Z && currentDoubleProton < dripNuc[i].Z )
+              if ( np == 1 && otherIsotope->Z == currentIsotope->Z && currentDoubleProton < currentIsotope->Z )
                 {
-                  dripNuc[i].s_2n = dripNuc[j].ME - dripNuc[i].ME + 2*meN;
+                  currentIsotope->s_2n = otherIsotope->ME - currentIsotope->ME + 2*meN;
 
-                  if ( dripNuc[i].s_2n < 0.0 )
+                  if ( currentIsotope->s_2n < 0.0 )
                     {
                       dripFile << std::fixed
-                               << std::setw(nucleonWidth) << dripNuc[i].N    << " "
-                               << std::setw(nucleonWidth) << dripNuc[i].Z    << " "
-                               << std::setw(dripWidth) << dripNuc[i].s_2n << "\n"
-                               << std::setw(nucleonWidth) << dripNuc[i].N    << " "
-                               << std::setw(nucleonWidth) << dripNuc[i].Z+1  << " "
-                               << std::setw(dripWidth) << dripNuc[i].s_2n << std::endl;
+                               << std::setw(nucleonWidth) << currentIsotope->N    << " "
+                               << std::setw(nucleonWidth) << currentIsotope->Z    << " "
+                               << std::setw(dripWidth) << currentIsotope->s_2n << "\n"
+                               << std::setw(nucleonWidth) << currentIsotope->N    << " "
+                               << std::setw(nucleonWidth) << currentIsotope->Z+1  << " "
+                               << std::setw(dripWidth) << currentIsotope->s_2n << std::endl;
 
                       ++currentDoubleProton;
                     }
                 }
 
               /// S(2p) = M(Z-2,N) - M(Z,N) + 2*M(1,0)
-              if ( np == 3 && dripNuc[j].N == dripNuc[i].N && currentDoubleNeutron < dripNuc[i].N )
+              if ( np == 3 && otherIsotope->N == currentIsotope->N && currentDoubleNeutron < currentIsotope->N )
                 {
-                  dripNuc[i].s_2p = dripNuc[j].ME - dripNuc[i].ME + 2*meP;
+                  currentIsotope->s_2p = otherIsotope->ME - currentIsotope->ME + 2*meP;
 
-                  if ( dripNuc[i].s_2p < 0.0 )
+                  if ( currentIsotope->s_2p < 0.0 )
                     {
-                      if ( dripNuc[i].N != previousDoubleNeutron )
+                      if ( currentIsotope->N != previousDoubleNeutron )
                         {
                           dripFile << std::fixed
                                    << std::setw(nucleonWidth) << previousDoubleNeutron+1 << " "
                                    << std::setw(nucleonWidth) << previousDoubleProton    << " "
-                                   << std::setw(dripWidth) << dripNuc[i].s_2p         << std::endl;
+                                   << std::setw(dripWidth) << currentIsotope->s_2p         << std::endl;
                         }
 
                       dripFile << std::fixed
-                               << std::setw(nucleonWidth) << dripNuc[i].N    << " "
-                               << std::setw(nucleonWidth) << dripNuc[i].Z    << " "
-                               << std::setw(dripWidth) << dripNuc[i].s_2p << std::endl;
+                               << std::setw(nucleonWidth) << currentIsotope->N    << " "
+                               << std::setw(nucleonWidth) << currentIsotope->Z    << " "
+                               << std::setw(dripWidth) << currentIsotope->s_2p << std::endl;
 
                       ++currentDoubleNeutron;
-                      previousDoubleProton  = dripNuc[i].Z;
-                      previousDoubleNeutron = dripNuc[i].N;
+                      previousDoubleProton  = currentIsotope->Z;
+                      previousDoubleNeutron = currentIsotope->N;
                     }
                 }
             }
         }
-
-      ++i;
     }
 
   file.close();

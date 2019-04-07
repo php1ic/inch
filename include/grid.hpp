384bede315267f61de74ad537a91a9456d0b334a
Ian Cullen <ian.james.cullen@gmail.com>
2019-04-07 17:11:26 +0100

    Document the Grid class

--- include/grid.hpp
+++ include/grid.hpp
@@ -1,3 +1,13 @@
+/**
+ *
+ * \class Grid
+ *
+ * \brief A square grid to help with positioning
+ *
+ * When adding new elements to the chart it can be useful to draw a square grid
+ * to aid with positioning
+ *
+ */
 #ifndef GRID_HPP
 #define GRID_HPP
 
@@ -7,23 +17,24 @@
 class Grid
 {
 public:
-  // Constructors
-  // default
   Grid() = default;
-  // copy
+
   Grid(const Grid& Grid) = default;
-  // move
   Grid(Grid&& Grid) noexcept = default;
 
-  // Assignment
-  // copy
   Grid& operator=(const Grid& Grid) = default;
-  // move
   Grid& operator=(Grid&& Grid) noexcept = default;
 
-  // Destructor
   ~Grid() noexcept = default;
 
+  /**
+   * Output the necessary EPS code to create the grid
+   *
+   * \param outFile An ostream pointing to the file curretly being created
+   * \param width The width of the chart in 'isotope' units i.e. The square representing an isotope is 1 unit
+   * \param height The height of the chart in 'iostope' units i.e. The square representing an isotope is 1 unit
+   * \param spacing The spacing of the square grid
+   */
   void EPSDrawGrid(std::ostream& outFile, const int width, const int height, const int spacing = 5) const;
 };
 

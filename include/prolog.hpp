eae5ba43b14a2d430bf51c3eb57563c5e3ce6da4
Ian Cullen <ian.james.cullen@gmail.com>
2019-05-25 12:52:33 +0100

    Document the Prolog class

--- include/prolog.hpp
+++ include/prolog.hpp
@@ -1,3 +1,12 @@
+/**
+ *
+ * \class Prolog
+ *
+ * \brief Set up the definitions and functions that will be used to create the chart
+ *
+ * To make smaller files, and create easier to read 'code', we define various functions, methods
+ * and constants for all type. Use this class to do that.
+ */
 #ifndef PROLOG_HPP
 #define PROLOG_HPP
 
@@ -12,35 +21,58 @@ struct tm;
 class Prolog
 {
 public:
-  // Constructors
-  // default
   explicit Prolog(const int size) : chart_size(size) { setTime(std::time(nullptr)); }
 
-  // copy
   Prolog(const Prolog&) = default;
-  // move
-  Prolog(Prolog&&) = default;
+  Prolog(Prolog&&)      = default;
 
-  // Assignment
-  // move
   Prolog& operator=(Prolog&&) = default;
-  // copy
   Prolog& operator=(const Prolog&) = default;
 
-  // Destructor
   ~Prolog() = default;
 
+  /// Get the date and time that chart is created (i.e. runtime)
   inline auto getTime() const { return std::put_time(now, "%Y-%m-%dT%H:%M:%S"); }
 
+  /**
+   * Write the functions needed to create the EPS file
+   *
+   * \param The output file stream representing the chart
+   * \param An instance of the Option class containing the chart options
+   *
+   * \return Nothing
+   */
   void EPSWriteProlog(std::ofstream& outFile, Options& draw) const;
+
+  /**
+   * Write the functions needed to create the TikZ file
+   *
+   * \param The output file stream representing the chart
+   * \param An instance of the Option class containing the chart options
+   *
+   * \return Nothing
+   */
   void TIKZWriteProlog(std::ofstream& outFile /*, std::unique_ptr<Options> &draw*/) const;
+
+  /**
+   * Write the functions needed to create the SVG file
+   *
+   * \param The output file stream representing the chart
+   * \param An instance of the Option class containing the chart options
+   *
+   * \return Nothing
+   */
   void SVGWriteProlog(std::ofstream& outFile, Options& draw) const;
 
 private:
+  /// The date and time at runtime
   mutable std::tm* now   = nullptr;
+  /// The size of the chart in 'iostope units'
   mutable int chart_size = 0;
+  /// How rounded are the 'squares' representing an isotope, 0=square, 1=circle
   mutable double curve   = 0.25;
 
+  /// Write the current date and time into the member variable
   inline void setTime(const std::time_t theTime) const { now = std::localtime(&theTime); }
 };
 

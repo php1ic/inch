0da2ba36010cdad7f7f1e7a885d0e7266e017e12
Ian Cullen <ian.james.cullen@gmail.com>
2019-05-25 13:33:52 +0100

    Document the UI class

--- include/ui.hpp
+++ include/ui.hpp
@@ -1,3 +1,8 @@
+/**
+ * \class UI
+ *
+ * \brief A place for user interaction functionality
+ */
 #ifndef UI_HPP
 #define UI_HPP
 
@@ -14,35 +19,75 @@
 class UI
 {
 public:
-  // Default constructor
   explicit UI(std::vector<Nuclide> _table, Options& _options) : table(std::move(_table)), options(_options) {}
 
-  // Copy
-  UI(const UI& other) = default;
-  // Move
+  UI(const UI& other)     = default;
   UI(UI&& other) noexcept = default;
 
-  // Copy assignment
   UI& operator=(const UI& other) = delete;
-  // Move assignment
   UI& operator=(UI&& other) noexcept = delete;
 
-  // Destructor
   virtual ~UI() noexcept = default;
 
+  /**
+   * ?????
+   *
+   * \param Nothing
+   *
+   * \return Nothing
+   */
   void getOptions();
+
+  /**
+   * Parent function that just runs other question functions
+   *
+   * \param Nothing
+   *
+   * \return Nothing
+   */
   void askQuestions();
 
 private:
+  /// Mass table data used to validate answers to questions
   mutable std::vector<Nuclide> table;
 
+  /// Which properties are being used to create the chart
   Options& options;
 
-  // Helper function, refactor and inherit?
+  /**
+   * Set the upper/lower limits on N or Z
+   *
+   * \param The value to set a limit on [Zmin,Zmax,Nmin,Nmax]
+   *
+   * \return Nothing
+   */
   void setExtreme(const std::string& limit) const;
 
+  /**
+   * Should the chart display measured, theoretical or both types of isotopes
+   *
+   * \param Nothing
+   *
+   * \return Nothing
+   */
   void selectChartType() const;
+
+  /**
+   * Which isotopic property should the chart be coloured by
+   *
+   * \param Nothing
+   *
+   * \return Nothing
+   */
   void selectChartColour() const;
+
+  /**
+   * How much of the chart should be drawn
+   *
+   * \param Nothing
+   *
+   * \return Nothing
+   */
   void selectChartSelection() const;
 };
 

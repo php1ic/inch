4a0b6c4f99f586a99d36b46b905fac414f4616d6
Ian Cullen <ian.james.cullen@gmail.com>
2020-03-27 14:45:13 +0000

    Move code coverage config to it's own file
    I've not got this to work yet, but I think it's almost there so lets
    not just delete everything.

---
+++ cmake/CodeCoverage.cmake
@@ -0,0 +1,22 @@
+function(enable_codecoverage)
+  add_library(coverage_config INTERFACE)
+
+  option(INCH_CODE_COVERAGE "Enable coverage reporting" OFF)
+  if(INCH_CODE_COVERAGE AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+    message(STATUS "Checking code coverage")
+    # Add required flags (GCC & LLVM/Clang)
+    target_compile_options(coverage_config INTERFACE
+      -O0        # no optimization
+      -g         # generate debug info
+      --coverage # sets all required flags
+      )
+    if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.13)
+      target_link_options(coverage_config INTERFACE --coverage)
+    else()
+      target_link_libraries(coverage_config INTERFACE --coverage)
+    endif()
+  endif()
+
+  target_link_libraries(${PROJECT_NAME} PUBLIC coverage_config)
+
+endfunction()

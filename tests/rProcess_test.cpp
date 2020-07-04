d1cb9576ffd41017d2cf2cea6172d6c68b314ca3
Ian Cullen <ian.james.cullen@gmail.com>
2020-07-04 17:42:04 +0100

    Add some tests to the rProcess class

--- tests/rProcess_test.cpp
+++ tests/rProcess_test.cpp
@@ -1,3 +1,23 @@
+#include "inch/rProcess.hpp"
+
 #include <catch2/catch.hpp>
 
-#include "inch/rProcess.hpp"
+const Limits l;
+rProcess rproc(l);
+
+TEST_CASE("Z value is larger than start of r-process", "[rProcess]")
+{
+  REQUIRE(rproc.isZHighEnough() == true);
+
+  auto isZset = rproc.limits.setZmax(20);
+
+  REQUIRE((isZset && rproc.isZHighEnough() == false));
+}
+
+TEST_CASE("Data file is set", "[rProcess]")
+{
+  const std::string theFile{ "random.file" };
+  rproc.setRProcessFile(theFile);
+
+  REQUIRE(rproc.file == theFile);
+}

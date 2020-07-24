f818a4b16d88b03cdbfbd6fb414d58c5b88fbd19
Ian Cullen <ian.james.cullen@gmail.com>
2020-07-24 14:58:34 +0100

    Add a test fir the Partition class

--- tests/partition_test.cpp
+++ tests/partition_test.cpp
@@ -1,3 +1,11 @@
+#include "inch/partition.hpp"
+
 #include <catch2/catch.hpp>
 
-#include "inch/partition.hpp"
+Partition testPartition(ChartColour::GS_DECAYMODE);
+
+TEST_CASE("Get the colour", "[Partition]")
+{
+  testPartition.setDefaultColours();
+  REQUIRE_THAT(testPartition.getColour("EC"), Catch::Matches("orange"));
+}

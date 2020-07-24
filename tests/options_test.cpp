c306896da165cdbc8fd689953f9eb5791302e27b
Ian Cullen <ian.james.cullen@gmail.com>
2020-07-24 14:38:49 +0100

    Add some tests to the options class

--- tests/options_test.cpp
+++ tests/options_test.cpp
@@ -1,3 +1,24 @@
+#include "inch/options.hpp"
+
 #include <catch2/catch.hpp>
 
-#include "inch/options.hpp"
+Options opt;
+
+TEST_CASE("Set the filetype", "[Options]")
+{
+  REQUIRE(opt.filetype == FileType::EPS);
+
+  opt.setFileType("tikz");
+  REQUIRE(opt.filetype == FileType::TIKZ);
+
+  opt.setFileType("svg");
+  REQUIRE(opt.filetype == FileType::SVG);
+}
+
+
+TEST_CASE("String/Symbol to Int", "[Options]")
+{
+  REQUIRE(opt.StringToInt("C") == 6);
+  REQUIRE(opt.StringToInt("He") == 2);
+  REQUIRE(opt.StringToInt("Ag") == 47);
+}

c83a1ae68316e50f5792cd5c29c23de6ed1d35a0
Ian Cullen <ian.james.cullen@gmail.com>
2020-07-24 15:54:59 +0100

    Start testing the Prolog class
    Fixed minor typo in the EPS prolog, but it caused everthing to be
    shifted to correct alignment.

--- tests/prolog_test.cpp
+++ tests/prolog_test.cpp
@@ -1,3 +1,30 @@
+#include "inch/prolog.hpp"
+
 #include <catch2/catch.hpp>
 
-#include "inch/prolog.hpp"
+const Limits limits;
+
+TEST_CASE("Creation time", "[Prolog]")
+{
+  // This seems overly complicated
+
+  // Get the time using inch
+  Prolog startBit(5, limits);
+
+  // Get the time ... raw
+  auto theTime = std::time(nullptr);
+  auto t       = *std::localtime(&theTime);
+
+  // Convert inch time to a string
+  std::ostringstream oss;
+  oss << startBit.getTime();
+  auto when = oss.str();
+
+  // Convert the raw time to a string
+  std::ostringstream ss;
+  ss << std::put_time(&t, "%Y-%m-%dT%H:%M:%S");
+  auto now = ss.str();
+
+  // Compare the strings
+  REQUIRE_THAT(when, Catch::Matches(now));
+}

ef16698748ebcedbfa267b1fa5a57b03cac8252e
Ian Cullen <ian.james.cullen@gmail.com>
2020-08-28 22:30:43 +0100

    Don't build fmt as a library during compilation of 'all'
    fmt was meant to be header only from the start, missed the option.

--- cmake/ExternalLibraries.cmake
+++ cmake/ExternalLibraries.cmake
@@ -1,6 +1,6 @@
 function(add_external_libraries project_name)
   # Use the {fmt} library. It will be part of C++20
-  add_subdirectory(external/fmt)
+  add_subdirectory(external/fmt EXCLUDE_FROM_ALL)
   target_link_libraries(project_options INTERFACE fmt::fmt-header-only)
 
   # Need to link the filesystem library in older compiler versions

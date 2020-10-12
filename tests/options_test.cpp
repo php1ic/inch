0b2612d9efe996fa32ba348402d9ca39fe853ecb
Ian Cullen <ian.james.cullen@gmail.com>
2020-10-12 22:01:40 +0100

    Add some more tests to the option class

--- tests/options_test.cpp
+++ tests/options_test.cpp
@@ -14,4 +14,84 @@ TEST_CASE("Set the filetype", "[Options]")
 
   opt.setFileType("svg");
   REQUIRE(opt.filetype == FileType::SVG);
+
+  opt.setFileType("eps");
+  REQUIRE(opt.filetype == FileType::EPS);
+}
+
+
+TEST_CASE("Output file is sanitised", "[Options]")
+{
+  Options options;
+
+  SECTION("Extension is removed if given, EPS")
+  {
+    options.outfile  = "chart.extension";
+    options.filetype = FileType::EPS;
+
+    REQUIRE(options.validateOutputFilename());
+    REQUIRE(options.outfile == "chart.eps");
+  }
+
+  SECTION("Extension is removed if given, SVG")
+  {
+    options.outfile  = "chart.extension";
+    options.filetype = FileType::SVG;
+
+    REQUIRE(options.validateOutputFilename());
+    REQUIRE(options.outfile == "chart.svg");
+  }
+
+  SECTION("Extension is removed if given, TIKZ")
+  {
+    options.outfile  = "chart.extension";
+    options.filetype = FileType::TIKZ;
+
+    REQUIRE(options.validateOutputFilename());
+    REQUIRE(options.outfile == "chart.tex");
+  }
+
+  SECTION("Try to output to a directory fall back to defalt name")
+  {
+    std::filesystem::create_directory("test_dir");
+    options.outfile  = "test_dir";
+    options.filetype = FileType::EPS;
+
+    REQUIRE(options.validateOutputFilename());
+    REQUIRE(options.outfile == "chart.eps");
+  }
+}
+
+
+TEST_CASE("Don't overwrite exising file", "[Options]")
+{
+  Options options;
+
+  SECTION("Unless it's called chart")
+  {
+    options.outfile  = "chart";
+    options.filetype = FileType::EPS;
+
+    options.setOutputFilename();
+    REQUIRE(options.outfile == "chart.eps");
+  }
+}
+
+TEST_CASE("Input options are valid", "[Options]")
+{
+  Options options;
+
+  SECTION("No values are given")
+  {
+    std::map<std::string, std::string> values{};
+
+    REQUIRE_FALSE(options.checkInputFileOptions(values));
+  }
+
+  SECTION("Not enough values are given")
+  {
+    std::map<std::string, std::string> values{ { "section", "1" }, { "type", "0" } };
+
+    REQUIRE_FALSE(options.checkInputFileOptions(values));
+  }
 }
